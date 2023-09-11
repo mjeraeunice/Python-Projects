2ce892b4920f851d041aefc13274cf7d37de0cd4
mjeraeunice <mjeraeunice@gmail.com>
2023-09-11 23:42:46 +0300

    added custom views

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from .views import add_to_cart, view_cart, update_cart, remove_item, empty_cart
+from . import views
 
 urlpatterns = [
-    path('add_to_cart/<int:id>/', add_to_cart, name='add_to_cart'),
-    path('product_cart/', view_cart, name='view_cart'),
-    path('update_cart/<int:id>/',update_cart, name='update_cart'),
-    path("remove_item/<int:id>/", remove_item, name = "remove_item"),
-    path("empty/", empty_cart, name="empty_cart"),
+    path('view-cart/', views.view_cart, name='view_cart'),
+    path('update-cart/<int:id>/', views.update_cart, name='update_cart'),
+    path('remove-item/<int:id>/', views.remove_item, name='remove_item'),
+    path('empty-cart/', views.empty_cart, name='empty_cart'),
+    path('add-to-cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
 ]
+
--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product_Cart
 from datetime import datetime
 from .models import Product
+from django.contrib import messages
+from django.urls import reverse
+from order.models import Order  
+from .forms import CartUploadForm  
 
 def view_cart(request):
     product_cart = Product_Cart.objects.all()
@@ -14,7 +18,6 @@ def view_cart(request):
 
     return render(request, "cart/view_cart.html", {"product_cart": product_cart, "total_cart_price": total_cart_price})
 
-
 def update_cart(request, id):
     if request.method == 'POST':
         quantity = int(request.POST.get('quantity', 1))
@@ -26,13 +29,11 @@ def update_cart(request, id):
             cart_item.delete()
     return redirect('view_cart')
 
-
-
 def remove_item(request, id):
     cart_item = Product_Cart.objects.get(id=id)
     cart_item.delete()
-
     return redirect('view_cart')
+
 def empty_cart(request):
     Product_Cart.objects.all().delete()
     return redirect("view_cart")
@@ -40,13 +41,47 @@ def empty_cart(request):
 def add_to_cart(request, id):
     product = Product.objects.get(id=id)
 
-    cart_item = Product_Cart (
-        product_name = product.name,
-        product_price = product.price,
-        product_image = product.image,
-        product_quantity = 1,
-        date_added=datetime.now(),
-    )
-    cart_item.save()
+    cart_item_exists = Product_Cart.objects.filter(product_name=product.name).exists()
 
-    return redirect("products_list")
+    if not cart_item_exists:
+        cart_item = Product_Cart (
+            product_name = product.name,
+            product_price = product.price,
+            product_image = product.image,
+            product_quantity = 1,
+            date_added=datetime.now(),
+        )
+        cart_item.save()
+        
+        messages.success(request, f"{product.name} added to cart successfully.")
+    else:
+        messages.warning(request, f"{product.name} is already in your cart.")
+
+    return redirect(reverse("products_list"))
+
+def checkout_view(request):
+    if request.method == 'POST':
+        form = CartUploadForm(request.POST)
+        if form.is_valid():
+            order = Order.objects.create(
+                user=request.user,  
+                order_date=datetime.now(),
+            )
+
+            product_cart = Product_Cart.objects.all()
+
+            for item in product_cart:
+                order.order_items.create(
+                    product_name=item.product_name,
+                    product_price=item.product_price,
+                    product_quantity=item.product_quantity,
+                )
+
+            Product_Cart.objects.all().delete()
+
+            return render(request, "cart/checkout_success.html", {"order": order})
+
+    else:
+        form = CartUploadForm()
+
+    return render(request, "cart/checkout.html", {"form": form})
